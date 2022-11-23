86d46cc8cf8c4b5f5fdf29217581f41cdfd31f72
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2022-11-24 08:44:43 +0900

    [level 0] Title: 로그인 성공?, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120883. 로그인 성공？/로그인 성공？.py
@@ -0,0 +1,11 @@
+def solution(id_pw, db):
+    if(id_pw in db):
+        return 'login'
+    for i in db:
+        if(id_pw[0]==i[0]):
+            return 'wrong pw'    
+        else:
+            pass
+    if(id_pw not in db):
+        return 'fail'
+            
