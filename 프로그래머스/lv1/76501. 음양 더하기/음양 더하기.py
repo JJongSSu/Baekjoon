3fb91cc682194ab56de157797bb3c8e19b59e4f7
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-03 10:10:47 +0900

    [level 1] Title: 음양 더하기, Time: 0.13 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -1,8 +1,9 @@
 def solution(absolutes, signs):
-    answer=[]
-    for i in range(len(signs)):
-        if(signs[i]==True):
-            answer.append(absolutes[i])
-        else:
-            answer.append(0-absolutes[i])
-    return sum(answer)
+    # answer=[]
+    # for i in range(len(signs)):
+    #     if(signs[i]==True):
+    #         answer.append(absolutes[i])
+    #     else:
+    #         answer.append(-absolutes[i])
+    # return sum(answer)
+    return sum(absolutes if sign else -absolutes for absolutes, sign in zip(absolutes, signs))
