b3c8232295164420ac8e29124921ca6dad346b15
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-04 16:50:02 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,15 +1,23 @@
 def solution(left, right):
-    a = list(range(left, right+1))
-    c=[]
-    for i in a:
-        b=[]
-        for j in range(1,i+1):
-            if(i%j==0):
-                b.append(j)
-            else:
-                pass
-        if(len(b)%2==0):
-            c.append(True)
+    # 제곱수는 약수의 개수가 홀수
+    answer=0
+    for i in range(left,right+1):
+        if int(i**(0.5))==i**(0.5):
+            answer-=i
         else:
-            c.append(False)
-    return sum(a if c else -a for a, c in zip(a,c)) 
+            answer+=i
+    return answer
+
+    # c=[]
+    # for i in range(left, right+1):
+    #     b=[]
+    #     for j in range(1,i+1):
+    #         if(i%j==0):
+    #             b.append(j)
+    #         else:
+    #             pass
+    #     if(len(b)%2==0):
+    #         c.append(True)
+    #     else:
+    #         c.append(False)
+    # return sum(a if c else -a for a, c in zip(a,c)) 
