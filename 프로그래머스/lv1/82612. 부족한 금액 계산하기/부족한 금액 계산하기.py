15ee4159b15599b588324ef948965ae39244b883
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2023-01-05 09:56:07 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,4 +1,4 @@
 def solution(price, money, count):
-    if(money>=sum(range(count+1))*price):
-        return 0
-    return sum(range(count+1))*price - money
+    return max(0,sum(range(count+1))*price - money)
+
+# max(0,price*(count+1)*count//2-money)
