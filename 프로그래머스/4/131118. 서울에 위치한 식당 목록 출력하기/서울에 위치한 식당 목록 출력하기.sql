bba9bb7a15321b109efcb0830dcbca5560c63282
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2025-04-28 21:32:27 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,30 +1,8 @@
 -- 코드를 입력하세요
-# SELECT 
-#     i.REST_ID, 
-#     i.REST_NAME, 
-#     i.FOOD_TYPE, 
-#     i.FAVORITES, 
-#     i.ADDRESS, 
-#     round(avg(r.REVIEW_SCORE),2) as SCORE 
-# from REST_INFO i
-# inner join REST_REVIEW r using(REST_ID)
-# where i.ADDRESS like '%서울%'
-# group by i.REST_ID
-# order by SCORE DESC, FAVORITES DESC
-SELECT
-    i.REST_ID,
-    i.REST_NAME,
-    i.FOOD_TYPE,
-    i.FAVORITES,
-    i.ADDRESS,
-    ROUND(AVG(r.REVIEW_SCORE), 2) AS SCORE
-FROM
-    REST_INFO i INNER JOIN REST_REVIEW r
-    ON i.REST_ID = r.REST_ID
-WHERE
-    i.ADDRESS LIKE '서울%'
-GROUP BY
-    i.REST_ID
-ORDER BY
-    SCORE DESC,
-    FAVORITES DESC
+SELECT i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, 
+    round(avg(r.REVIEW_SCORE),2) as SCORE 
+from REST_INFO i
+inner join REST_REVIEW r using(REST_ID)
+where i.ADDRESS like '서울%'
+group by i.REST_ID
+order by SCORE DESC, FAVORITES DESC
