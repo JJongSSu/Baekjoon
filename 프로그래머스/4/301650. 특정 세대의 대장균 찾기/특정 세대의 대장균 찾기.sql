0ce84b8561dc7066c56184792d300f472a695446
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2025-05-01 21:22:56 +0900

    [level 4] Title: 특정 세대의 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
+++ 프로그래머스/4/301650. 특정 세대의 대장균 찾기/특정 세대의 대장균 찾기.sql
@@ -1,11 +1,17 @@
 -- 코드를 작성해주세요
-select ID
-from ECOLI_DATA
-where 
-(case 
-    when PARENT_ID is null then 1
-    when PARENT_ID in (select ID from ECOLI_DATA where PARENT_ID is null) then 2
-    when PARENT_ID in (select ID from ECOLI_DATA where PARENT_ID in (select ID from ECOLI_DATA where PARENT_ID is null)) then 3
-    else 4
-end) = 3
+with recursive cte_generation as (
+    select ID, PARENT_ID, 1 as generation
+    from ECOLI_DATA
+    where PARENT_ID is null
+    
+    union all
+    
+    select e.ID, e.PARENT_ID, g.generation + 1
+    from ECOLI_DATA e
+    join cte_generation g on e.PARENT_ID = g.ID
+    where g.generation < 4  # 재귀 단계 설정(기본 1000)
+)
+
+select ID from cte_generation
+where generation = 3
 order by ID
