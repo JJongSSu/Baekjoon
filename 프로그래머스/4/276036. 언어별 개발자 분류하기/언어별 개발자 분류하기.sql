3b56f5128d751c986ca8d7068be3e5992c729b83
JJongSSu <112380397+JJongSSu@users.noreply.github.com>
2025-05-14 22:47:33 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,39 +1,37 @@
 -- 코드를 작성해주세요
-with SUM_CTE as (
-    select 
-    (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End') FRONT_END,
-    (select CODE from SKILLCODES where NAME = 'Python') PYTHON,
-    (select CODE from SKILLCODES where NAME = 'C#') C_SHARP
-),
-DEVELOPERS_SKILL as (select 
-    case
-    when SKILL_CODE & FRONT_END and SKILL_CODE & PYTHON then 'A'
-    when SKILL_CODE & C_SHARP then 'B'
-    when SKILL_CODE & FRONT_END then 'C'
-    else null
-    end as GRADE,
-    ID,
-    EMAIL
-from DEVELOPERS, SUM_CTE)
-
-select * from DEVELOPERS_SKILL
-where GRADE is not null
-order by GRADE, ID
+SELECT
+        CASE 
+        WHEN GROUP_CONCAT(s.NAME) LIKE ("%Python%") AND GROUP_CONCAT(s.CATEGORY) LIKE("%Front%") THEN "A"
+        WHEN GROUP_CONCAT(s.NAME) LIKE ("%C#%") THEN "B"
+        WHEN GROUP_CONCAT(s.CATEGORY) LIKE ("%Front%") THEN "C"
+        END AS GRADE,
+        d.ID,
+        d.EMAIL
+FROM DEVELOPERS  AS d
+JOIN SKILLCODES AS s
+ON (d.SKILL_CODE & s.CODE)
+GROUP BY ID, EMAIL 
+HAVING GRADE IS NOT NULL
+ORDER BY GRADE ASC, ID ASC
 
 
+# with SUM_CTE as (
+#     select 
+#     (select sum(CODE) from SKILLCODES where CATEGORY = 'Front End') FRONT_END,
+#     (select CODE from SKILLCODES where NAME = 'Python') PYTHON,
+#     (select CODE from SKILLCODES where NAME = 'C#') C_SHARP
+# ),
+# DEVELOPERS_SKILL as (select 
+#     case
+#     when SKILL_CODE & FRONT_END and SKILL_CODE & PYTHON then 'A'
+#     when SKILL_CODE & C_SHARP then 'B'
+#     when SKILL_CODE & FRONT_END then 'C'
+#     else null
+#     end as GRADE,
+#     ID,
+#     EMAIL
+# from DEVELOPERS, SUM_CTE)
 
-# SELECT
-#         CASE 
-#         WHEN GROUP_CONCAT(B.NAME) LIKE ("%Python%") AND GROUP_CONCAT(B.CATEGORY) LIKE("%Front%") THEN "A"
-#         WHEN GROUP_CONCAT(B.NAME) LIKE ("%C#%") THEN "B"
-#         WHEN GROUP_CONCAT(B.CATEGORY) LIKE ("%Front%") THEN "C"
-#         END AS  GRADE ,
-#         A.ID,
-#         A.EMAIL
-
-# FROM DEVELOPERS  AS A
-# JOIN SKILLCODES AS B
-# ON (A.SKILL_CODE & B.CODE = B.CODE)
-# GROUP BY ID,EMAIL 
-# HAVING GRADE IS NOT NULL
-# ORDER BY GRADE ASC , ID ASC
+# select * from DEVELOPERS_SKILL
+# where GRADE is not null
+# order by GRADE, ID
